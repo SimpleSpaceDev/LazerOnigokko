7e3e79be1e5639c6170e95690308429806ecca84
Matchan <matchan0727ahaha@gmail.com>
2021-06-12 23:00:38 +0900

    とりあえず適当にログ追加

--- src/main/java/jp/simplespace/lazeronigokko/LazerOnigokko.java
+++ src/main/java/jp/simplespace/lazeronigokko/LazerOnigokko.java
@@ -7,7 +7,7 @@ public final class LazerOnigokko extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("プラグインを読み込んだよ☆");
     }
 
     @Override
