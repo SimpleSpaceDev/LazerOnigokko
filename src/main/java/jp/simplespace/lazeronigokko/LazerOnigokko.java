9577f4491d6a4e8e73dce4f4d1211244355d0bc3
Matchan <matchan0727ahaha@gmail.com>
2021-06-12 22:58:23 +0900

    Initial commit

---
+++ src/main/java/jp/simplespace/lazeronigokko/LazerOnigokko.java
@@ -0,0 +1,17 @@
+package jp.simplespace.lazeronigokko;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LazerOnigokko extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
