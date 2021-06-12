0af109d48e09e3a415bfc555247c8bd83a30761e
Matchan <matchan0727ahaha@gmail.com>
2021-06-13 00:04:27 +0900

    サイズを少し拡大

--- src/main/java/jp/simplespace/lazeronigokko/Util/Lazer.java
+++ src/main/java/jp/simplespace/lazeronigokko/Util/Lazer.java
@@ -4,7 +4,7 @@ import org.bukkit.*;
 
 public class Lazer {
     public static void spawnParticle(World world, Location loc){
-        Particle.DustOptions dust = new Particle.DustOptions(Color.RED,1);
+        Particle.DustOptions dust = new Particle.DustOptions(Color.RED,1.5f);
         world.spawnParticle(Particle.REDSTONE,loc,0,dust);
     }
 }
