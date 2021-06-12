83b13bb25d84b020b4d86d3b6f72cf2721d060e1
Matchan <matchan0727ahaha@gmail.com>
2021-06-12 23:04:33 +0900

    無効化時のログを追加。

--- src/main/java/jp/simplespace/lazeronigokko/LazerOnigokko.java
+++ src/main/java/jp/simplespace/lazeronigokko/LazerOnigokko.java
@@ -13,5 +13,6 @@ public final class LazerOnigokko extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("プラグインが去ったよ☆");
     }
 }
