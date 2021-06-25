5be0ed2c9a278f7852a0c7a71d74371b665a29d1
Matchan <matchan0727ahaha@gmail.com>
2021-06-25 22:51:39 +0900

    画面(GUI)の種類の列挙型

---
+++ src/main/java/jp/simplespace/lazeronigokko/GUI/GuiType.java
@@ -0,0 +1,10 @@
+package jp.simplespace.lazeronigokko.GUI;
+
+import jp.simplespace.lazeronigokko.GUI.Menu.GunSelect;
+import org.bukkit.Material;
+
+public enum GuiType {
+    NONE,
+    GUN_SELECT;
+
+}
