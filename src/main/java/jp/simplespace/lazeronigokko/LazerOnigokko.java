4e4ac256a2ec8f7b254d8087be3a527c585020a8
Matchan <matchan0727ahaha@gmail.com>
2021-06-25 22:50:41 +0900

    guiコマンドの登録とタブ補完の登録。 初期化処理の追加。 イベントリスナーの登録。

--- src/main/java/jp/simplespace/lazeronigokko/LazerOnigokko.java
+++ src/main/java/jp/simplespace/lazeronigokko/LazerOnigokko.java
@@ -1,10 +1,16 @@
 package jp.simplespace.lazeronigokko;
 
 import jp.simplespace.lazeronigokko.Commands.Setup;
+import jp.simplespace.lazeronigokko.GUI.Gui;
+import jp.simplespace.lazeronigokko.GUI.GuiListener;
+import jp.simplespace.lazeronigokko.Listener.JoinLeaveListener;
 import jp.simplespace.lazeronigokko.Listener.NormalLazerListener;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LazerOnigokko extends JavaPlugin {
@@ -18,15 +24,25 @@ public final class LazerOnigokko extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         plugin=this;
+        PluginManager pm = getServer().getPluginManager();
         //コンフィグ処理
         saveDefaultConfig();
         config = getConfig();
         //コマンド登録
         getCommand("setup").setExecutor(new Setup());
+        getCommand("gui").setExecutor(new jp.simplespace.lazeronigokko.Commands.Gui());
         //コマンドタブ補完登録
         getCommand("setup").setTabCompleter(new Setup());
+        getCommand("gui").setTabCompleter(new jp.simplespace.lazeronigokko.Commands.Gui());
+        //初期化処理
+        Gui.init();
+        for(Player player : Bukkit.getOnlinePlayers()){
+            player.kickPlayer(prefix+"プラグインが読み込み、若しくは再読み込みされたため、\nあなたはキックされました。");
+        }
         //リスナー登録
-        getServer().getPluginManager().registerEvents(new NormalLazerListener(),this);
+        pm.registerEvents(new NormalLazerListener(),this);
+        pm.registerEvents(new JoinLeaveListener(),this);
+        pm.registerEvents(new GuiListener(),this);
         getLogger().info("プラグインを読み込んだよ☆");
     }
 
